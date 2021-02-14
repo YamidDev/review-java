30ca94b2a6a78646b4c0295d34df4c48f867d0f3
YamidDev <yamidcuetomazo@gmail.com>
2021-02-14 15:57:06 -0500

    add system numeric class

---
+++ src/SystemNumeric.java
@@ -0,0 +1,23 @@
+import javax.swing.*;
+
+public class SystemNumeric {
+    public static void main(String[] args) {
+        String numberStr = JOptionPane.showInputDialog(null, "type a number");
+        int number = 0;
+        try {
+            number = Integer.parseInt(numberStr);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Err you need type a number");
+            main(args);
+            System.exit(0);
+        }
+        printMessage(number, "Binary", Integer.toBinaryString(number));
+        printMessage(number, "Octal", Integer.toOctalString(number));
+        printMessage(number, "Hex", Integer.toHexString(number));
+    }
+
+    public static void printMessage(int number, String type, String value) {
+        String message = "the value of " + number + " to " + type + " is equal to " + value;
+        JOptionPane.showMessageDialog(null, message);
+    }
+}
