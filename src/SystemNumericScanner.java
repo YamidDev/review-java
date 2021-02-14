5104265de792bec15f024c65d89ee3ba1fe12bfb
YamidDev <yamidcuetomazo@gmail.com>
2021-02-14 16:26:11 -0500

    use class scanner for read lines into the console

---
+++ src/SystemNumericScanner.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class SystemNumericScanner {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("type a number");
+        String numberStr = sc.nextLine();
+        int number = 0;
+        try {
+            number = Integer.parseInt(numberStr);
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Err you need type a number");
+            main(args);
+            System.exit(0);
+        }
+        printMessage(number, "Binary", Integer.toBinaryString(number));
+        printMessage(number, "Octal", Integer.toOctalString(number));
+        printMessage(number, "Hex", Integer.toHexString(number));
+    }
+
+    public static void printMessage(int number, String type, String value) {
+        String message = "the value of " + number + " to " + type + " is equal to " + value;
+        System.out.println(message);
+    }
+}
