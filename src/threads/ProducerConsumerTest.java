207f23004d4e5023e553459f204c4194797c1c92
YamidDev <yamidcuetomazo@gmail.com>
2021-03-29 08:36:29 -0500

    Example of threads

---
+++ src/threads/ProducerConsumerTest.java
@@ -0,0 +1,9 @@
+package threads;
+
+public class ProducerConsumerTest {
+    public static void main(String[] args) {
+        Drop drop = new Drop();
+        (new Thread(new Producer(drop))).start();
+        (new Thread(new Consumer(drop))).start();
+    }
+}
